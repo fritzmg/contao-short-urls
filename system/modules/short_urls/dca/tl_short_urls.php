ad73716a4a6ce80c78f977bb06c736742b05ec77
fritzmg <email@spikx.net>
2016-03-07 12:00:18 +0100

    fixed bug for older Contao versions

--- system/modules/short_urls/dca/tl_short_urls.php
+++ system/modules/short_urls/dca/tl_short_urls.php
@@ -336,10 +336,31 @@ class tl_short_urls extends Backend
 		$options = array( '' => $GLOBALS['TL_LANG']['tl_short_urls']['noDomain'] );
 
 		// get the root pages and their dns settings
-		if( ( $objPages = \PageModel::findPublishedRootPages() ) !== null )
-			while( $objPages->next() )
-				if( $objPages->dns )
-					$options[$objPages->id] = $objPages->dns;
+		if( version_compare( VERSION, '3.5', '>=' ) )
+		{
+			if( ( $objPages = \PageModel::findPublishedRootPages() ) !== null )
+				while( $objPages->next() )
+					if( $objPages->dns )
+						$options[$objPages->id] = $objPages->dns;			
+		}
+		else
+		{
+			$t = \PageModel::$strTable;
+			$arrColumns = array("$t.type=?");
+
+			if (!BE_USER_LOGGED_IN)
+			{
+				$time = time();
+				$arrColumns[] = "($t.start='' OR $t.start<='$time') AND ($t.stop='' OR $t.stop>'" . ($time + 60) . "') AND $t.published='1'";
+			}
+
+			$objPages = \PageModel::findBy($arrColumns, 'root');
+
+			if( $objPages !== null )
+				while( $objPages->next() )
+					if( $objPages->dns )
+						$options[$objPages->id] = $objPages->dns;
+		}
 
 		// return the options
 		return $options;
